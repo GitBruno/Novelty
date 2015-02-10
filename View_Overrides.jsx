85fb7449d519d0d0013d3a9ebf31c74cf843ca85
Bruno Herfst <mail@brunoherfst.com>
2015-02-11 09:36:18 +1100

    Added view overrides
    A handy toggle from Marc Autret

---
+++ View_Overrides.jsx
@@ -0,0 +1,32 @@
+#target InDesign;
+
+/***********************************************************************/
+/*                                                                     */
+/*      ShowHideLocalFormatting ::   Show/hide local formatting        */
+/*                                                                     */
+/*      [Ver: 1.1]    [Author: Marc Autret]     [Modif: 05/25/10]      */
+/*      [Lang: EN]    [Req: InDesign CS4]       [Creat: 05/09/10]      */
+/*                                                                     */
+/*      Installation:                                                  */
+/*                                                                     */
+/*      1) Place the current file into Scripts/Scripts Panel/          */
+/*                                                                     */
+/*      2) Run InDesign, open a document containing texts              */
+/*                                                                     */
+/*      3) Exec script from Window > Automatisation > Scripts          */
+/*         (double-click on the script name)                           */
+/*                                                                     */
+/*      Bugs & Feedback : marc{at}indiscripts{dot}com                  */
+/*                        www.indiscripts.com                          */
+/*                                                                     */
+/***********************************************************************/
+
+if( app.documents.length &&
+	app.activeDocument.textPreferences.enableStylePreviewMode^= 1 &&
+	app.layoutWindows.length &&
+	app.activeWindow.constructor==LayoutWindow){
+
+		app.activeWindow.screenMode = ScreenModeOptions.previewOff;
+		// UPDATE [100525] - THX to ptruskier
+		app.activeWindow.overprintPreview = false;
+	}
