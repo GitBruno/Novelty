b711e28b6b29e0e3abb9cdeff3870bda4bf6c3dd
Bruno Herfst <hello@brunoherfst.com>
2016-04-26 15:18:05 +1000

    Added support for spreads
    I know this is ugly code :( but don’t feel like refactoring right now,
    it just needs to work today.

--- Files_2_Pages.jsx
+++ Files_2_Pages.jsx
@@ -96,9 +96,12 @@ function myDisplayDialog(){
 				var myPercentField = percentEditboxes.add({editValue:myValues("myPercentField", 100)});
 			}
 			with(myFitButtons2 = radiobuttonGroups.add()){
-				var myFitMarginRadio = radiobuttonControls.add( { staticLabel : 'To margins'} );
+				var myFitPageMarginRadio = radiobuttonControls.add( { staticLabel : 'To page margins'} );
 				var myFitPageRadio = radiobuttonControls.add( { staticLabel : 'To page' } );
-				var myFitBleedRadio = radiobuttonControls.add( { staticLabel : 'To bleed' } );
+				var myFitPageBleedRadio = radiobuttonControls.add( { staticLabel : 'To page bleed' } );
+				var myFitSpreadMarginRadio = radiobuttonControls.add( { staticLabel : 'To spread margins'} );
+				var myFitSpreadRadio = radiobuttonControls.add( { staticLabel : 'To spread' } );
+				var myFitSpreadBleedRadio = radiobuttonControls.add( { staticLabel : 'To spread bleed' } );
 				myFitButtons2.selectedButton = myValues("myFitButtons2", 0);
 			}
 		}
@@ -111,18 +114,21 @@ function myDisplayDialog(){
 	var myResult = myDialog.show();
 
     if(myResult == true){
-        var change_master  = myDoc.masterSpreads.item(myChange_master.selectedIndex);
-        var objectStyle    = myDoc.objectStyles.item(myObjectStyle.selectedIndex);
-        var selected_pageNo  = insert_page.selectedIndex;
-        var myPercent      = myPercentField.editValue;
-        var myFitPercent   = myFitPercentRadio.checkedState;
-        var myFitScaleDown = myFitScaleDownCheckbox.checkedState;
-        var myFit          = myFitRadio.checkedState;
-        var myFitMargin    = myFitMarginRadio.checkedState;
-        var myFitPage      = myFitPageRadio.checkedState;
-        var myFitBleed     = myFitBleedRadio.checkedState;
-        var myFitCenterContent  = myFitCenterContentCheckbox.checkedState;
-        var myFitFrameToContent = myFitFrameToContentCheckbox.checkedState;
+        var change_master    = myDoc.masterSpreads.item(myChange_master.selectedIndex);
+        var objectStyle      = myDoc.objectStyles.item(myObjectStyle.selectedIndex);
+        var selected_page_or_spreadNo  = insert_page.selectedIndex;
+        var myPercent        = myPercentField.editValue;
+        var myFitPercent     = myFitPercentRadio.checkedState;
+        var myFitScaleDown   = myFitScaleDownCheckbox.checkedState;
+        var myFit            = myFitRadio.checkedState;
+        var myFitPageMargin    = myFitPageMarginRadio.checkedState;
+        var myFitPage          = myFitPageRadio.checkedState;
+        var myFitPageBleed     = myFitPageBleedRadio.checkedState;
+        var myFitSpreadMargin    = myFitSpreadMarginRadio.checkedState;
+        var myFitSpread          = myFitSpreadRadio.checkedState;
+        var myFitSpreadBleed     = myFitSpreadBleedRadio.checkedState;
+        var myFitCenterContent   = myFitCenterContentCheckbox.checkedState;
+        var myFitFrameToContent  = myFitFrameToContentCheckbox.checkedState;
 
         if (mySelectedLayer.selectedIndex == list_of_layers.length-1){
             var selectedLayer = myDoc.layers.add();
@@ -148,12 +154,17 @@ function myDisplayDialog(){
         // saves JSON which is converted to string to activeDocument label
         app.activeDocument.insertLabel("PFOP-Settings", mySettings.toSource());
 
+        var useSpreads = false;
+        if(myFitButtons2.selectedButton >= 3) {
+        	useSpreads = true;
+    	}
+
         // we have to reverse the order when we are placing before or after a static page
         myFiles.reverse();
 
         myDialog.destroy();
 
-        myPlaceImages(myFiles, myFitPercent, myPercent, myFit, myFitMargin, myFitPage, myFitBleed, myFitCenterContent, myFitFrameToContent, myFitScaleDown, selected_pageNo, change_master, before_after, selectedLayer, objectStyle);
+        myPlaceImages(myFiles, useSpreads, myFitPercent, myPercent, myFit, myFitPageMargin, myFitPage, myFitPageBleed, myFitSpreadMargin, myFitSpread, myFitSpreadBleed, myFitCenterContent, myFitFrameToContent, myFitScaleDown, selected_page_or_spreadNo, change_master, before_after, selectedLayer, objectStyle);
     } else {
         myDialog.destroy();
         exit();
@@ -183,10 +194,9 @@ function getINDPageCount(inddFile) {
 	return pagecount;
 }
 
-function myPlaceImages(myFiles, myFitPercent, myPercent, myFit, myFitMargin, myFitPage, myFitBleed, myFitCenterContent, myFitFrameToContent, myFitScaleDown, selected_pageNo, change_master, before_after, selectedLayer, objectStyle){
+function myPlaceImages(myFiles, useSpreads, myFitPercent, myPercent, myFit, myFitPageMargin, myFitPage, myFitPageBleed, myFitSpreadMargin, myFitSpread, myFitSpreadBleed, myFitCenterContent, myFitFrameToContent, myFitScaleDown, selected_page_or_spreadNo, change_master, before_after, selectedLayer, objectStyle){
 	var filesPlaced = 0;
 	var filecount = myFiles.length;
-	//var hack_page = selected_pageNo;
 
 	for (var i=0; i<=myFiles.length-1; i++){
 		var myCounter = 1;
@@ -209,76 +219,127 @@ function myPlaceImages(myFiles, myFitPercent, myPercent, myFit, myFitMargin, myF
 			if( (myCounter > 1) && (before_after == 1) ){ // if we go more then once over the loop we are placing PDFs or Images
 			    //I need to reverse the PDF pages if I want to place them after a static page.
 				//Solution: update the before_after page
-				var selected_page = myPage;
+				var selected_page_or_spread = myPageOrSpread;
 			} else if( (myCounter > 1) && (before_after == 0) ){
 			    alert("SORRY!\nI can't place multi-page files like PDFs using 'before' yet. This functionality still needs to be worked out. Please choose 'after' or email mail@brunoherfst.com");
 			    break;
 			} else {
-			    var selected_page = myDoc.pages.item(selected_pageNo);
-			}
-
-			if (before_after == 0){
-				var myPage = myDoc.pages.add(LocationOptions.BEFORE,selected_page);
-			} else {
-				var myPage = myDoc.pages.add(LocationOptions.AFTER,selected_page);
-			}
-			filesPlaced++;
-
-			myPage.appliedMaster = change_master;
-
-			var myTopMargin = myPage.marginPreferences.top;
-			var myBottomMargin = myPage.marginPreferences.bottom;
-			var bleed = myDoc.documentPreferences.documentBleedTopOffset; //(can be made more specific, good for now);
-
-            if( ((before_after == 1) && (myPage.side == PageSideOptions.leftHand)  && (filecount %2 == 0))
-             || ((before_after == 1) && (myPage.side == PageSideOptions.rightHand) && (filecount %2 == 1))
-             || ((before_after == 0) && (myPage.side == PageSideOptions.leftHand))
-             ){
-                var startPage = 0;
-            } else {
-                var startPage = 1;
-            }
-
-			if(filesPlaced % 2 == startPage){
-			        var myOutsideMargin = myPage.marginPreferences.left;
-					var myInsideMargin = myPage.marginPreferences.right;
-					var bleedLeft  = bleed;
-					var bleedRight = 0;
-			}
-			else{
-				var myOutsideMargin = myPage.marginPreferences.right;
-				var myInsideMargin  = myPage.marginPreferences.left;
-				var bleedLeft  = 0;
-				var bleedRight = bleed;
-			}
-
-			if (myFitMargin){
-				var myX1 = myInsideMargin;
-				var myY1 = myTopMargin;
-				var myX2 = myDoc.documentPreferences.pageWidth  - myOutsideMargin;
-				var myY2 = myDoc.documentPreferences.pageHeight - myBottomMargin;
-			}
-			if (myFitPage){
-				var myX1 = 0;
-				var myY1 = 0;
-				var myX2 = myDoc.documentPreferences.pageWidth;
-				var myY2 = myDoc.documentPreferences.pageHeight;
-			}
-			if (myFitBleed){
-			    if(myPage.side == PageSideOptions.SINGLE_SIDED){
-			        var myX1 = 0-bleed;
-				    var myY1 = 0-bleed;
-				    var myX2 = myDoc.documentPreferences.pageWidth  + bleed;
-				    var myY2 = myDoc.documentPreferences.pageHeight + bleed;
-			    } else {
-			        var myX1 = 0-bleedLeft;
-				    var myY1 = 0-bleed;
-				    var myX2 = myDoc.documentPreferences.pageWidth  + bleedRight;
-				    var myY2 = myDoc.documentPreferences.pageHeight + bleed;
+				if(useSpreads) {
+					var selected_page_or_spread = myDoc.pages.item(selected_page_or_spreadNo).parent;
+				} else {
+					var selected_page_or_spread = myDoc.pages.item(selected_page_or_spreadNo);
 				}
 			}
 
-			myRectangle = myPage.rectangles.add(selectedLayer, undefined, undefined, {geometricBounds:[myY1, myX1, myY2, myX2],appliedObjectStyle:objectStyle});
+			if(useSpreads) {
+				if (before_after == 0){
+					var myPageOrSpread = myDoc.spreads.add(LocationOptions.BEFORE,selected_page_or_spread);
+				} else {
+					var myPageOrSpread = myDoc.spreads.add(LocationOptions.AFTER,selected_page_or_spread);
+				}
+				// THIS SCRIPT DOES NOT WORJ ON DOC WITH SPREADS MORE THEN TWO PAGES
+				for(var p = 0; p < myPageOrSpread.pages.length; p++) {
+					myPageOrSpread.pages[p].appliedMaster = change_master;
+				}
+
+				var myTopMargin = myPageOrSpread.pages[0].marginPreferences.top;
+				var myBottomMargin = myPageOrSpread.pages[0].marginPreferences.bottom;
+				var bleed = myDoc.documentPreferences.documentBleedTopOffset; //(can be made more specific, good for now);
+
+			} else {
+				if (before_after == 0){
+					var myPageOrSpread = myDoc.pages.add(LocationOptions.BEFORE,selected_page_or_spread);
+				} else {
+					var myPageOrSpread = myDoc.pages.add(LocationOptions.AFTER,selected_page_or_spread);
+				}
+				myPageOrSpread.appliedMaster = change_master;
+
+				var myTopMargin = myPageOrSpread.marginPreferences.top;
+				var myBottomMargin = myPageOrSpread.marginPreferences.bottom;
+				var bleed = myDoc.documentPreferences.documentBleedTopOffset; //(can be made more specific, good for now);
+
+			}
+			
+			filesPlaced++;
+
+            if(useSpreads){
+            	var myOutsideMargin = myPageOrSpread.pages[myPageOrSpread.pages.length-1].marginPreferences.right;
+				var myInsideMargin  = myPageOrSpread.pages[0].marginPreferences.left;
+				var bleedLeft  = bleed;
+				var bleedRight = bleed;
+            } else {
+            	if( ((before_after == 1) && (myPageOrSpread.side == PageSideOptions.leftHand)  && (filecount %2 == 0))
+	             || ((before_after == 1) && (myPageOrSpread.side == PageSideOptions.rightHand) && (filecount %2 == 1))
+	             || ((before_after == 0) && (myPageOrSpread.side == PageSideOptions.leftHand))
+	             ){
+	                var startPage = 0;
+	            } else {
+	                var startPage = 1;
+	            }
+            	if(filesPlaced % 2 == startPage){
+			        var myOutsideMargin = myPageOrSpread.marginPreferences.left;
+					var myInsideMargin = myPageOrSpread.marginPreferences.right;
+					var bleedLeft  = bleed;
+					var bleedRight = 0;
+				}
+				else{
+					var myOutsideMargin = myPageOrSpread.marginPreferences.right;
+					var myInsideMargin  = myPageOrSpread.marginPreferences.left;
+					var bleedLeft  = 0;
+					var bleedRight = bleed;
+				}
+            }
+
+			if(useSpreads){
+				var mySpreadBounds = new Array (myPageOrSpread.pages[0].bounds[0], myPageOrSpread.pages[0].bounds[1], myPageOrSpread.pages[myPageOrSpread.pages.length-1].bounds[2], myPageOrSpread.pages[myPageOrSpread.pages.length-1].bounds[3] );
+				if (myFitSpreadMargin){
+					var myX1 = myInsideMargin;
+					var myY1 = myTopMargin;
+					var myX2 = mySpreadBounds[3] - myOutsideMargin;
+					var myY2 = mySpreadBounds[2] - myBottomMargin;
+				}
+				if (myFitSpread){
+					var myX1 = mySpreadBounds[1];
+					var myY1 = mySpreadBounds[0];
+					var myX2 = mySpreadBounds[3];
+					var myY2 = mySpreadBounds[2];
+				}
+				if (myFitSpreadBleed){
+			        var myX1 = mySpreadBounds[1] - bleed;
+				    var myY1 = mySpreadBounds[0] - bleed;
+				    var myX2 = mySpreadBounds[3] + bleed;
+				    var myY2 = mySpreadBounds[2] + bleed;
+				}
+			} else {
+				if (myFitPageMargin){
+					var myX1 = myInsideMargin;
+					var myY1 = myTopMargin;
+					var myX2 = myDoc.documentPreferences.pageWidth  - myOutsideMargin;
+					var myY2 = myDoc.documentPreferences.pageHeight - myBottomMargin;
+				}
+				if (myFitPage){
+					var myX1 = 0;
+					var myY1 = 0;
+					var myX2 = myDoc.documentPreferences.pageWidth;
+					var myY2 = myDoc.documentPreferences.pageHeight;
+				}
+				if (myFitPageBleed){
+				    if(myPageOrSpread.side == PageSideOptions.SINGLE_SIDED){
+				        var myX1 = 0-bleed;
+					    var myY1 = 0-bleed;
+					    var myX2 = myDoc.documentPreferences.pageWidth  + bleed;
+					    var myY2 = myDoc.documentPreferences.pageHeight + bleed;
+				    } else {
+				        var myX1 = 0-bleedLeft;
+					    var myY1 = 0-bleed;
+					    var myX2 = myDoc.documentPreferences.pageWidth  + bleedRight;
+					    var myY2 = myDoc.documentPreferences.pageHeight + bleed;
+					}
+				}
+			}
+			
+
+			myRectangle = myPageOrSpread.rectangles.add(selectedLayer, undefined, undefined, {geometricBounds:[myY1, myX1, myY2, myX2],appliedObjectStyle:objectStyle});
 			//And place the file in the textframe
 			myRectangle.place(myFiles[i]);
 
@@ -287,7 +348,7 @@ function myPlaceImages(myFiles, myFitPercent, myPercent, myFit, myFitMargin, myF
 				if(myFitPercent){
 					myRectangle.allGraphics[0].horizontalScale=myPercent;
 					myRectangle.allGraphics[0].verticalScale=myPercent;
-				} else if(myFitMargin || myFitPage){
+				} else if(myFitPageMargin || myFitPage || myFitSpreadMargin || myFitSpread){
 					myRectangle.fit(FitOptions.proportionally);
 				}
 
@@ -315,7 +376,7 @@ function myPlaceImages(myFiles, myFitPercent, myPercent, myFit, myFitMargin, myF
 						}
 					} else {
 						if(myRectangle.pdfs[0].pdfAttributes.pageNumber == myFirstPage){
-							myPage.remove();
+							myPageOrSpread.remove();
 							filesPlaced--;
 							myBreak = true;
 						}
@@ -324,7 +385,7 @@ function myPlaceImages(myFiles, myFitPercent, myPercent, myFit, myFitMargin, myF
 					//indesign file
 					app.importedPageAttributes.pageNumber = myCounter+1;
 					if(myCounter == inddpagelength+1){
-						myPage.remove();
+						myPageOrSpread.remove();
 						filesPlaced--;
 						myBreak = true;
 					}
@@ -338,6 +399,12 @@ function myPlaceImages(myFiles, myFitPercent, myPercent, myFit, myFitMargin, myF
 		} // end while loop
 	}
 	restoreOriginalSettings();
-	alert("Done\n" + filesPlaced + " pages inserted.");
+
+	if(useSpreads){
+		alert("Done\n" + filesPlaced + " spreads inserted.");
+	} else {
+		alert("Done\n" + filesPlaced + " pages inserted.");	
+	}
+	
 }
 
